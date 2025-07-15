0f36e0f099c57e727cb79d481f4d35b80aa55d27
ikana82 <ikanurasih82@gmail.com>
2025-07-15 14:01:43 +0700

    Create and Install router pages

---
+++ start/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AdminLayout from "./layouts/AdminLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/auth",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "login",
+        element: <div>Login</div>,
+      },
+      {
+        path: "register",
+        element: <div>Register</div>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
