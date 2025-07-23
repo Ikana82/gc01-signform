425437458980342fd7d8c5cea849e65c4ba402f6
ikana82 <ikanurasih82@gmail.com>
2025-07-23 14:58:56 +0700

    Menambahkan folder start untuk preview

---
+++ start/src/layouts/MainLayout.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { auth } from "../configs/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { Outlet, useNavigate } from "react-router";
+
+export default function MainLayout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (!user) {
+        navigate("/auth/login");
+      }
+    });
+  }, []);
+
+  return (
+    <>
+      <header>-- Home Side --</header>
+      <Outlet />
+    </>
+  );
+}
